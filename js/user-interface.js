fa6c464585cdf86a6e070bad0cb74c7e49c3c6ed
Gathua Kiragu <kiragugathua@gmail.com>
2017-03-17 17:32:14 +0300

    index better appearance

--- js/user-interface.js
+++ js/user-interface.js
@@ -1,8 +1,6 @@
-$(document).ready(function () {
-    $('#submit').click(function (event) {
-        event.preventDefault();
-        var gitHubName = $('#username').val();
-        $('#username').val("");
-        $('#solution').text("Your Github UserName Is " + gitHubName + ".");
-    });
+$('.message a').click(function () {
+    $('form').animate({
+        height: "toggle"
+        , opacity: "toggle"
+    }, "slow");
 });
