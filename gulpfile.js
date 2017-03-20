a24cb1d9604185959a56df40f16eba2d5397beab
Gathua Kiragu <kiragugathua@gmail.com>
2017-03-20 08:11:29 +0300

    This is the final commit

--- gulpfile.js
+++ gulpfile.js
@@ -3,47 +3,58 @@ var browserify = require('browserify');
 var source = require('vinyl-source-stream');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
+var browserSync = require('browser-sync').create();
+var sass = require('gulp-sass');
 gulp.task('bowerCSS', function () {
-    return gulp.src(lib.ext('css').files).pipe(concat('vendor.css')).pipe(gulp.dest('./build/css'));
+return gulp.src(lib.ext('css').files).pipe(concat('vendor.css')).pipe(gulp.dest('./build/css'));
 });
 gulp.task('myTask', function () {
-    console.log('hello gulp');
+console.log('hello gulp');
 });
 //This concats the code so that it's short'
 gulp.task('concatInterface', function () {
-    return gulp.src(['./js/*-interface.js']) //    array of js files to be concated
-        .pipe(concat('allConcat.js')) //makes a folder called allConcat.js
-        .pipe(gulp.dest('./tmp')); // destination is the tmp folder
+return gulp.src(['./js/*-interface.js']) //    array of js files to be concated
+.pipe(concat('allConcat.js')) //makes a folder called allConcat.js
+.pipe(gulp.dest('./tmp')); // destination is the tmp folder
 });
 //When browserify is run it will also run concat at the same time but it has to be predefined
 gulp.task('jsBrowserify', ['concatInterface'], function () {
-    return browserify({
-        entries: ['./tmp/allConcat.js']
-    }).bundle().pipe(source('app.js')).pipe(gulp.dest('./build/js'));
+return browserify({
+entries: ['./tmp/allConcat.js']
+}).bundle().pipe(source('app.js')).pipe(gulp.dest('./build/js'));
 });
 //combined functionality of jsBrowserify into minify so that it works at once. This is beautiful
 gulp.task("minifyscripts", ["jsBrowserify"], function () {
-    return gulp.src("./build/js/app.js").pipe(uglify()).pipe(gulp.dest("./build/js"));
+return gulp.src("./build/js/app.js").pipe(uglify()).pipe(gulp.dest("./build/js"));
 });
 // combines functionality of build and clean
 gulp.task("clean", function () {
-    return del(['build', 'tmp']);
+return del(['build', 'tmp']);
 });
 var jshint = require('gulp-jshint');
 gulp.task('jshint', function () {
-    return gulp.src(['js/*.js']).pipe(jshint()).pipe(jshint.reporter('default'));
+return gulp.src(['js/*.js']).pipe(jshint()).pipe(jshint.reporter('default'));
 });
 var lib = require('bower-files')();
 gulp.task('bowerJS', function () {
-    return gulp.src(lib.ext('js').files).pipe(concat('vendor.min.js')).pipe(gulp.dest('./build/js'));
+return gulp.src(lib.ext('js').files).pipe(concat('vendor.min.js')).pipe(gulp.dest('./build/js'));
+});
+gulp.task('serve', function() {
+browserSync.init({
+server: {
+baseDir: "./",
+index: "index.html"
+}
+});
+gulp.watch(['js/*.js'], ['jsBuild']);
 });
 gulp.task('bower', ['bowerJS', 'bowerCSS']);
 gulp.task('build', ['clean'], function () {
-    if (buildProduction) {
-        gulp.start('minifyScripts');
-    }
-    else {
-        gulp.start('jsBrowserify');
-    }
-    gulp.start('bower');
+if (buildProduction) {
+gulp.start('minifyScripts');
+}
+else {
+gulp.start('jsBrowserify');
+}
+gulp.start('bower');
 });
